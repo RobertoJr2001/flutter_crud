f847bb654ee3a47725e5d9b7fb38d178c9e6333c
RobertoJr2001 <junionascimento2001@gmail.com>
2020-12-22 04:22:20 -0300

    Fix: vscode

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -6,6 +6,6 @@ const DUMMY_USERS = {
     name: 'apu',
     email: 'apu@india.com',
     avatarUrl:
-        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+        "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png",
   )
 };
